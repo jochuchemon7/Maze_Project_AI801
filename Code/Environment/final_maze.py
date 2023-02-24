2ae14ae609db28b6cee4b6a4cbc664c005194c45
Josue Perez Gomez <37346269+jochuchemon7@users.noreply.github.com>
2023-02-23 22:56:32 -0600

    Update final_maze.py

--- Code/Environment/final_maze.py
+++ Code/Environment/final_maze.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# https://en.wikipedia.org/wiki/Maze_generation_algorithm  (PRIM'S ALGORITHM)
+
 class CreateMaze:
     def __init__(self, row=20, col=20):
         self.nrow, self.ncol = row, col
